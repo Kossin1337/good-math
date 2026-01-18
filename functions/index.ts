9fd05a5b2e219d03dd667727f3d15cb27f36559c
kossin1337 <piotrswobodzinski@gmail.com>
2026-01-18 18:29:40 +0100

    server shift

--- functions/index.ts
+++
@@ -1,8 +0,0 @@
-import { onRequest } from "firebase-functions/v2/https";
-import app from "./app";
-
-// You can also configure CORS here instead of using the cors() middleware.
-export const api = onRequest(
-  { cors: ["http://localhost:5173", /web\.app$/, /firebaseapp\.com$/] },
-  app
-);
